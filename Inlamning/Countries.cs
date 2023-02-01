760730b1c92c545fe6e5bcecd67a3825dd0bd2de
amne1600 <amne1600@student.miun.se>
2023-02-02 00:41:32 +0100

    #1.1

---
+++ Inlamning/Countries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlamning
+{
+    /// <summary>
+    /// The Countries that are allowed!
+    /// </summary>
+    public enum Countries
+    {
+        Saudi_Arabia,
+        Iran,
+        USA,
+        England,
+        Sweden,
+        Antigue_And_Barbuda,
+        Kuwait,
+        Mexico,
+        Colombia,
+    }
+}
